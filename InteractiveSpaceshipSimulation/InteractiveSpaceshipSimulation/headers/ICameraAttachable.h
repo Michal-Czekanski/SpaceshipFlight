1472ea039447c01f73babfb10f4a25901aece192
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-09 00:50:48 +0100

    Remove reduntant "virtual" from getters

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/ICameraAttachable.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/ICameraAttachable.h
@@ -12,9 +12,9 @@ protected:
 	glm::vec3 vectorForward;
 	glm::vec3 vectorTop;
 public:
-	virtual glm::vec3 getPosition();
-	virtual glm::quat getRotationQuat();
-	virtual glm::vec3 getVectorForward();
-	virtual glm::vec3 getVectorTop();
+	glm::vec3 getPosition();
+	glm::quat getRotationQuat();
+	glm::vec3 getVectorForward();
+	glm::vec3 getVectorTop();
 };
 
