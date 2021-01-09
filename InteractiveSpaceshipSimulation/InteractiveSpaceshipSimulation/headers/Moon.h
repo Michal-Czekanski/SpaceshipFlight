7027e2aef3a348aa0d32cc73dc42998a1805ac7b
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-09 19:53:17 +0100

    Add Moon ctor

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Moon.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Moon.h
@@ -1,5 +1,17 @@
 #pragma once
-class Moon
+
+#include "Orbitable.h"
+#include "Planet.h"
+
+class Moon: public Orbitable
 {
+public:
+	/// <summary>
+	/// Creates Moon which orbits around given Planet.
+	/// </summary>
+	/// <param name="orbitAround">Planet to orbit around.</param>
+	/// <param name="orbitPlaneVec2">Second of the two vectors defining rotation plane. First one is calculated as vector pointing from Planet to Moon.</param>
+	Moon(glm::vec3 position, glm::quat rotationQuat, glm::vec3 vectorForward, glm::vec3 vectorTop, obj::Model model, glm::vec3 topInModelSpace, glm::vec3 forwardInModelSpace,
+		glm::vec3 scale, Planet* orbitAround, glm::vec3 orbitPlaneVec2, float orbitSpeed);
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Moon.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Moon.cpp
@@ -1,1 +1,7 @@
 #include "../headers/Moon.h"
+
+Moon::Moon(glm::vec3 position, glm::quat rotationQuat, glm::vec3 vectorForward, glm::vec3 vectorTop, obj::Model model, glm::vec3 topInModelSpace, 
+	glm::vec3 forwardInModelSpace, glm::vec3 scale, Planet* orbitAround, glm::vec3 orbitPlaneVec2, float orbitSpeed):
+	Orbitable(position, rotationQuat, vectorForward, vectorTop, model, topInModelSpace, forwardInModelSpace, scale, orbitAround, orbitPlaneVec2, orbitSpeed)
+{
+}
