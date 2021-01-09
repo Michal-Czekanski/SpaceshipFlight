3b618bd9a2ed74fa9e324aab2cae82d5f018cf71
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-09 13:09:37 +0100

    Add comments in Planet cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Planet.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Planet.h
@@ -6,9 +6,15 @@
 class Planet: public RenderableObject
 {
 private:
+	/// <summary>
+	/// Star around which this planet orbits.
+	/// </summary>
 	Star* orbitAround = NULL;
 
 public:
+	/// <summary>
+	/// Creates Planet which will not orbit around any star.
+	/// </summary>
 	Planet(glm::vec3 position, glm::quat rotationQuat, glm::vec3 vectorForward, glm::vec3 vectorTop, obj::Model model, glm::vec3 topInModelSpace, glm::vec3 forwardInModelSpace,
 		glm::vec3 scale);
 };
