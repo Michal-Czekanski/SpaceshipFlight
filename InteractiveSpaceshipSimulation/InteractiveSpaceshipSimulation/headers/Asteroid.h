947824928d52cf50956d31a0e0dca69966fd1e13
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-09 22:31:54 +0100

    Add Asteroid ctor

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Asteroid.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Asteroid.h
@@ -1,5 +1,14 @@
 #pragma once
-class Asteroid
+#include "RenderableObject.h"
+
+class Asteroid: public RenderableObject
 {
+private:
+	float speed;
+	glm::vec3 moveDirection;
+
+public:
+	Asteroid(glm::vec3 position, glm::quat rotationQuat, glm::vec3 vectorForward, glm::vec3 vectorTop, obj::Model model, glm::vec3 topInModelSpace, glm::vec3 forwardInModelSpace,
+		glm::vec3 scale, float speed, glm::vec3 moveDirection);
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Asteroid.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Asteroid.cpp
@@ -1,1 +1,9 @@
 #include "../headers/Asteroid.h"
+
+Asteroid::Asteroid(glm::vec3 position, glm::quat rotationQuat, glm::vec3 vectorForward, glm::vec3 vectorTop, obj::Model model, glm::vec3 topInModelSpace, glm::vec3 forwardInModelSpace,
+	glm::vec3 scale, float speed, glm::vec3 moveDirection) :
+	RenderableObject(position, rotationQuat, vectorForward, vectorTop, model, topInModelSpace, forwardInModelSpace, scale)
+{
+	this->speed = speed;
+	this->moveDirection = moveDirection;
+}
