42363e2e770983b06bdeef1ec9f70c8114e5dec6
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-10 00:46:45 +0100

    Add AsteroidField ctor and getter

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/AsteroidField.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/AsteroidField.h
@@ -1,5 +1,22 @@
 #pragma once
-class AsteroidField
-{
-};
 
+#include "ObjectInSpace.h"
+#include "Asteroid.h"
+#include <vector>
+#include "utils/Calculations.h"
+
+class AsteroidField: public ObjectInSpace
+{
+private:
+	glm::vec3 moveDirection;
+	std::vector<Asteroid*> asteroids;
+	int asteroidsCount;
+	glm::vec3 initialPosition;
+	glm::vec3 position;
+public:
+	AsteroidField(int asteroidCount, float asteroidFieldRadius, float asteroidSpeed, float minAsteroidScale, float maxAsteroidScale,
+		glm::vec3 position, glm::vec3 moveDirection, std::vector<obj::Model> asteroidModels,
+		glm::quat rotationQuat = glm::quat(), glm::vec3 vectorTop = glm::vec3(0, 0, 1));
+
+	std::vector<Asteroid*> getAsteroids();
+};
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/AsteroidField.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/AsteroidField.cpp
@@ -1,1 +1,27 @@
-#include "../headers/Asteroid.h"
+#include "../headers/AsteroidField.h"
+
+AsteroidField::AsteroidField(int asteroidCount, float asteroidFieldRadius, float asteroidSpeed, float minAsteroidScale, float maxAsteroidScale,
+	glm::vec3 position, glm::vec3 moveDirection, std::vector<obj::Model> asteroidModels,  glm::quat rotationQuat, glm::vec3 vectorTop):
+	ObjectInSpace(position, rotationQuat, moveDirection, vectorTop)
+{
+	this->moveDirection = moveDirection;
+	this->asteroidsCount = asteroidCount;
+	this->initialPosition = position;
+
+	for (int i = 0; i < asteroidCount; i++)
+	{
+		glm::vec3 asteroidPos = glm::ballRand(asteroidFieldRadius) + position;
+		glm::vec3 scale = glm::vec3(randomFloat(minAsteroidScale, maxAsteroidScale));
+		glm::quat rotationQuat = randomRotationQuat();
+		obj::Model asteroidModel = asteroidModels[randomInt(0, asteroidModels.size() - 1)];
+
+		Asteroid* asteroid = new Asteroid(asteroidPos, rotationQuat, glm::ballRand(1.0f), glm::ballRand(1.0f), asteroidModel,
+			glm::vec3(0, 1, 0), glm::vec3(0, 0, 1), scale, asteroidSpeed, moveDirection);
+		this->asteroids.push_back(asteroid);
+	}
+}
+
+std::vector<Asteroid*> AsteroidField::getAsteroids()
+{
+	return this->asteroids;
+}
