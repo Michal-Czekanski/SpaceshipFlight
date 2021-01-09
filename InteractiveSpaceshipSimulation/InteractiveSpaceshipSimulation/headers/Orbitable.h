22140696e763ad7c8d0e5c153884f046145023cf
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-09 19:39:37 +0100

    Add Orbitable cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Orbitable.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Orbitable.h
@@ -1,5 +1,54 @@
 #pragma once
-class Orbitable
+
+#include "RenderableObject.h"
+#include "utils/Calculations.h"
+
+class Orbitable: public RenderableObject
 {
+protected:
+	/// <summary>
+	/// Object around which this orbits.
+	/// </summary>
+	ObjectInSpace* orbitAround = NULL;
+
+	/// <summary>
+	/// Plane in which this object orbits. First vector defining this plane is direction vector pointing from orbitAround object's center to this object's center.
+	/// </summary>
+	glm::vec3 orbitPlane[2];
+
+	/// <summary>
+	/// Axis along which this object orbits around orbitAround object. It is orthogonal to the orbitPlane.
+	/// </summary>
+	glm::vec3 orbitAxis;
+
+	/// <summary>
+	/// Quat used to rotate this object around orbitAround object.
+	/// </summary>
+	glm::quat orbitQuat;
+
+	float orbitSpeed;
+	
+	/// <summary>
+	/// Vector pointing from orbit center to this object's initial position in world.
+	/// </summary>
+	glm::vec3 initPosRelativeToOrbitCenter;
+	/// <summary>
+	/// Vector pointing from orbit center to this object.
+	/// </summary>
+	glm::vec3 posRelativeToOrbitCenter;
+
+public:
+	/// <summary>
+	/// Creates object which orbits around other object.
+	/// </summary>
+	/// <param name="orbitAround">Object to orbit around.</param>
+	/// <param name="orbitPlaneVec2">Second of the two vectors defining rotation plane. First one is calculated as vector pointing from orbitAround object to this object.</param>
+	Orbitable(glm::vec3 position, glm::quat rotationQuat, glm::vec3 vectorForward, glm::vec3 vectorTop, obj::Model model, glm::vec3 topInModelSpace, glm::vec3 forwardInModelSpace, glm::vec3 scale,
+		ObjectInSpace* orbitAround, glm::vec3 orbitPlaneVec2, float orbitSpeed);
+
+	/// <summary>
+	/// Makes object orbit -> updates it's position based on orbitSpeed.
+	/// </summary>
+	void orbit();
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Orbitable.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Orbitable.cpp
@@ -1,1 +1,37 @@
 #include "../headers/Orbitable.h"
+
+
+Orbitable::Orbitable(glm::vec3 position, glm::quat rotationQuat, glm::vec3 vectorForward, glm::vec3 vectorTop, obj::Model model, glm::vec3 topInModelSpace, glm::vec3 forwardInModelSpace, glm::vec3 scale,
+	ObjectInSpace* orbitAround, glm::vec3 orbitPlaneVec2, float orbitSpeed):
+	RenderableObject(position, rotationQuat, vectorForward, vectorTop, model, topInModelSpace, forwardInModelSpace, scale)
+	
+{
+	if (orbitAround)
+	{
+		this->orbitAround = orbitAround;
+		this->orbitSpeed = orbitSpeed;
+
+		this->initPosRelativeToOrbitCenter = position - orbitAround->getPosition();
+		this->posRelativeToOrbitCenter = this->initPosRelativeToOrbitCenter;
+
+		this->orbitPlane[1] = glm::normalize(orbitPlaneVec2);
+		this->orbitPlane[0] = glm::normalize(this->posRelativeToOrbitCenter);
+
+		this->orbitAxis = glm::normalize(glm::cross(this->orbitPlane[0], this->orbitPlane[1]));
+	}
+	else 
+	{
+		this->orbitSpeed = 0.0f;
+	}
+	
+}
+
+void Orbitable::orbit()
+{
+	if (this->orbitAround)
+	{
+		this->orbitQuat = calculateRotationQuatLocalAxises(this->orbitQuat, this->orbitPlane[1], this->orbitAxis, this->orbitPlane[0], 0.0f, this->orbitSpeed, 0.0f);
+		this->posRelativeToOrbitCenter = this->orbitQuat * this->initPosRelativeToOrbitCenter;
+		this->position = this->posRelativeToOrbitCenter + this->orbitAround->getPosition();
+	}
+}
