fa1a6265b7c269370d8b7c18a2c1ff40bb43ffef
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-09 03:56:03 +0100

    Add Star ctor

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Star.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Star.h
@@ -1,5 +1,11 @@
 #pragma once
-class Star
+
+#include "RenderableObject.h"
+
+class Star : public RenderableObject
 {
+public:
+	Star(glm::vec3 position, glm::quat rotationQuat, glm::vec3 vectorForward,
+		glm::vec3 vectorTop, obj::Model model, glm::vec3 topInModelSpace, glm::vec3 forwardInModelSpace, glm::vec3 scale);
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Star.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Star.cpp
@@ -1,1 +1,8 @@
 #include "../headers/Star.h"
+
+Star::Star(glm::vec3 position, glm::quat rotationQuat, glm::vec3 vectorForward, glm::vec3 vectorTop, 
+	obj::Model model, glm::vec3 topInModelSpace, glm::vec3 forwardInModelSpace, glm::vec3 scale): 
+	RenderableObject(position, rotationQuat, vectorForward, vectorTop, model, topInModelSpace, forwardInModelSpace, scale)
+{
+
+}
