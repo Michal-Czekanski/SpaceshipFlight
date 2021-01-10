5b18d640c8d4fb3590cfb22c39c7400df8964e9b
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-10 02:25:43 +0100

    Implement Time cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Time.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Time.h
@@ -1,5 +1,63 @@
 #pragma once
+
+#include <chrono>
+
+using namespace std::chrono;
+
 class Time
 {
+private:
+	/// <summary>
+	/// In miliseconds.
+	/// </summary>
+	static long long measureStart;
+
+	/// <summary>
+	/// In miliseconds
+	/// </summary>
+	static long long lastFrameTime;
+
+	/// <summary>
+	/// In miliseconds
+	/// </summary>
+	static long long currentFrameTime;
+
+	/// <summary>
+	/// In miliseconds
+	/// </summary>
+	static long long deltaTime;
+
+	/// <summary>
+	/// In miliseconds
+	/// </summary>
+	static long long gameDuration;
+	static bool isRunning;
+public:
+	/// <summary>
+	/// Starts measuring time.
+	/// </summary>
+	static void start();
+
+	/// <summary>
+	/// Updates time (last and current frame time, delta time).
+	/// </summary>
+	static void update();
+
+	/// <summary>
+	/// Returns time passed since last frame in miliseconds.
+	/// </summary>
+	/// <returns></returns>
+	static long long getDeltaTime();
+
+	/// <summary>
+	/// Returns time passed since last frame in seconds.
+	/// </summary>
+	static float getDeltaTimeSec();
+
+	/// <summary>
+	/// Returns game duration in miliseconds
+	/// </summary>
+	/// <returns></returns>
+	static long long getGameDuration();
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Time.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Time.cpp
@@ -1,1 +1,48 @@
 #include "../headers/Time.h"
+
+long long Time::measureStart;
+long long Time::lastFrameTime;
+long long Time::currentFrameTime;
+long long Time::deltaTime;
+long long Time::gameDuration;
+bool Time::isRunning;
+
+void Time::start()
+{
+	if (!isRunning)
+	{
+		Time::isRunning = true;
+		Time::measureStart = duration_cast<microseconds>(high_resolution_clock::now().time_since_epoch()).count();
+		Time::lastFrameTime = 0;
+		Time::currentFrameTime = 0;
+		Time::deltaTime = 0;
+		Time::gameDuration = 0;
+	}
+}
+
+void Time::update()
+{
+	if (isRunning)
+	{
+		Time::lastFrameTime = currentFrameTime;
+		Time::currentFrameTime = duration_cast<microseconds>(high_resolution_clock::now().time_since_epoch()).count();
+		Time::deltaTime = currentFrameTime - lastFrameTime;
+		Time::gameDuration += Time::deltaTime;
+	}
+}
+
+long long Time::getDeltaTime()
+{
+	return Time::deltaTime;
+}
+
+float Time::getDeltaTimeSec()
+{
+	return Time::deltaTime / 1000.0f;
+}
+
+long long Time::getGameDuration()
+{
+	return Time::gameDuration;
+}
+
