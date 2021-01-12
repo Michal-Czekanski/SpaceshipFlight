977b0aedf80673ede94832ec0cd2e24924a423df
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-12 16:11:51 +0100

    Add ctor and update() in ShipLight cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/ShipLight.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/ShipLight.h
@@ -1,5 +1,33 @@
 #pragma once
+
+#include "glm.hpp"
+#include "ext.hpp"
+
+/// <summary>
+/// Ship's light is being emitted in a cone that's coming from ship's front.
+/// </summary>
 class ShipLight
 {
+private:
+	float lightConeHeight;
+	float lightConeBaseRadius;
+	glm::vec3 position;
+	glm::vec3 lightDirection;
+public:
+	/// <summary>
+	/// Creates ship's light.
+	/// </summary>
+	/// <param name="lightConeHeight"></param>
+	/// <param name="lightConeBaseRadius"></param>
+	/// <param name="position">Ship's position.</param>
+	/// <param name="lightDirection">Ship's front direction.</param>
+	ShipLight(float lightConeHeight, float lightConeBaseRadius, glm::vec3 position, glm::vec3 lightDirection);
+
+	/// <summary>
+	/// Updates ship's light data.
+	/// </summary>
+	/// <param name="position">Ship's position.</param>
+	/// <param name="lightDirection">Ship's direction.</param>
+	void update(glm::vec3 position, glm::vec3 lightDirection);
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/ShipLight.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/ShipLight.cpp
@@ -1,1 +1,15 @@
 #include "../../headers/data/ShipLight.h"
+
+ShipLight::ShipLight(float lightConeHeight, float lightConeBaseRadius, glm::vec3 position, glm::vec3 lightDirection)
+{
+	this->lightConeHeight = lightConeHeight;
+	this->lightConeBaseRadius = lightConeBaseRadius;
+	this->position = position;
+	this->lightDirection = lightDirection;
+}
+
+void ShipLight::update(glm::vec3 position, glm::vec3 lightDirection)
+{
+	this->position = position;
+	this->lightDirection = lightDirection;
+}
