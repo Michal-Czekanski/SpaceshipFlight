2d2839030aec885971b481dfef4c0246f24ef6a9
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-13 23:03:10 +0100

    Update star light pos after constructing it

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Star.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/Star.h
@@ -5,11 +5,11 @@
 class Star : public RenderableObject
 {
 private:
-	StarLight light;
+	StarLight *light;
 
 public:
 	Star(glm::vec3 position, ModelData &modelData, glm::vec3 scale, StarLight *light);
 
-	StarLight getLight();
+	StarLight* getLight();
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Star.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/Star.cpp
@@ -5,10 +5,11 @@
 Star::Star(glm::vec3 position, ModelData& modelData, glm::vec3 scale, StarLight* light):
 	RenderableObject(position, modelData, scale)
 {
-	this->light = *light;
+	this->light = light;
+	this->light->update(position);
 }
 
-StarLight Star::getLight()
+StarLight* Star::getLight()
 {
 	return this->light;
 }
