9a004573b4aeacc0812b077b2c24d12391d39178
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-20 17:54:27 +0100

    Implement DiscreteLOD cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/DiscreteLOD.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/DiscreteLOD.h
@@ -1,9 +1,23 @@
 #pragma once
 
+#include "glm.hpp"
+#include "ext.hpp"
+#include "../utils/objload.h"
+#include "../data/LevelOfDetail.h"
+
 /// <summary>
 /// Discrete Level Of Detail - based on distance from camera chooses which model to render: simplified or detailed.
 /// </summary>
 class DiscreteLOD
 {
+public:
+	/// <summary>
+	/// Decides which model should be used to render based on distance from camera.
+	/// </summary>
+	/// <param name="distanceFromCamera"></param>
+	/// <param name="simplifiedModel"></param>
+	/// <param name="detailedModel"></param>
+	/// <returns></returns>
+	obj::Model& whichModelUse(float distanceFromCamera, obj::Model& simplifiedModel, obj::Model& detailedModel);
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/DiscreteLOD.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/DiscreteLOD.cpp
@@ -1,1 +1,14 @@
 #include "../../headers/graphics_techniques/DiscreteLOD.h"
+
+obj::Model& DiscreteLOD::whichModelUse(float distanceFromCamera, obj::Model& simplifiedModel, obj::Model& detailedModel)
+{
+	LevelOfDetail levelOfDetail = LevelOfDetail(distanceFromCamera);
+	if (levelOfDetail.getDetailLevel() == LevelOfDetailEnum::High)
+	{
+		return detailedModel;
+	}
+	else if (levelOfDetail.getDetailLevel() == LevelOfDetailEnum::Low)
+	{
+		return simplifiedModel;
+	}
+}
