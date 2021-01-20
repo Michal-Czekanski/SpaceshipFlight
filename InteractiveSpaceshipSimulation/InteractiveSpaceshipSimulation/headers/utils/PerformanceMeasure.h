47f4088aa7118c27c697085cb1859c5060d8a7d1
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-20 23:15:46 +0100

    Implement PerformanceMeasure cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/utils/PerformanceMeasure.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/utils/PerformanceMeasure.h
@@ -1,5 +1,59 @@
 #pragma once
+
+#include "../Time.h"
+
 class PerformanceMeasure
 {
+private:
+	/// <summary>
+	/// Miliseconds per frame - better way of measuring performance than FPS.
+	/// </summary>
+	static float msPerFrame;
+
+	static int framesPerSecond;
+
+	static int framesCounter;
+
+	/// <summary>
+	/// Timer used to measure performance.
+	/// </summary>
+	static float timer;
+
+	/// <summary>
+	/// How much time performance measuring should take in seconds.
+	/// </summary>
+	static const float measureTime;
+
+	/// <summary>
+	/// Resets timer and measures. Should be called every second.
+	/// </summary>
+	static void reset();
+
+	static bool isRunning;
+
+	static void takeMeasures(int framesCount);
+
+public:
+	/// <summary>
+	/// Returns miliseconds needed to render frame. Good performance measure.
+	/// </summary>
+	static float getMsPerFrame();
+
+	/// <summary>
+	/// Updates performance measures like framesCounter.
+	/// </summary>
+	/// <returns></returns>
+	static void updatePerformanceMeasures(float deltaTime);
+
+	/// <summary>
+	/// Returns Frame Per Second. Bad performance measure.
+	/// </summary>
+	/// <returns></returns>
+	static int getFPS();
+
+	/// <summary>
+	/// Stars measuring performance.
+	/// </summary>
+	static void start();
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/utils/PerformanceMeasure.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/utils/PerformanceMeasure.cpp
@@ -1,1 +1,54 @@
 #include "../../headers/utils/PerformanceMeasure.h"
+
+float PerformanceMeasure::msPerFrame;
+int PerformanceMeasure::framesPerSecond;
+float PerformanceMeasure::timer;
+const float PerformanceMeasure::measureTime = 1.0f;
+
+void PerformanceMeasure::updatePerformanceMeasures(float deltaTime)
+{
+	if (PerformanceMeasure::isRunning)
+	{
+		PerformanceMeasure::timer -= deltaTime;
+		if (PerformanceMeasure::timer <= 0.0f)
+		{
+			PerformanceMeasure::takeMeasures(PerformanceMeasure::framesCounter);
+			PerformanceMeasure::reset();
+		}
+		PerformanceMeasure::framesCounter++;
+	}
+}
+
+void PerformanceMeasure::takeMeasures(int framesCount)
+{
+	PerformanceMeasure::framesPerSecond = framesCount;
+	PerformanceMeasure::msPerFrame = 1000.0f / framesCount;
+}
+
+void PerformanceMeasure::reset()
+{
+	PerformanceMeasure::timer = PerformanceMeasure::measureTime;
+	PerformanceMeasure::framesCounter = 0;
+}
+
+float PerformanceMeasure::getMsPerFrame()
+{
+	return PerformanceMeasure::msPerFrame;
+}
+
+int PerformanceMeasure::getFPS()
+{
+	return PerformanceMeasure::framesPerSecond;
+}
+
+void PerformanceMeasure::start()
+{
+	if (!isRunning)
+	{
+		PerformanceMeasure::msPerFrame = 0.0f;
+		PerformanceMeasure::framesCounter = 0;
+		PerformanceMeasure::framesPerSecond = 0;
+		PerformanceMeasure::isRunning = true;
+		PerformanceMeasure::timer = PerformanceMeasure::measureTime;
+	}
+}
