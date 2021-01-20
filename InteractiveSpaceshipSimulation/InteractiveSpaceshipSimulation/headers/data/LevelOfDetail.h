1aacdfab9312190bfb1934e0d9b137b354e1a862
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-20 17:37:10 +0100

    Implement LevelOfDetail class

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/LevelOfDetail.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/LevelOfDetail.h
@@ -1,5 +1,17 @@
 #pragma once
+enum class LevelOfDetailEnum
+{
+	Low, High
+};
+
 class LevelOfDetail
 {
+private:
+	LevelOfDetailEnum detailLevel;
+	const int highDetailMaxDistance = 150.0f;
+
+public:
+	LevelOfDetail(float distanceFromCamera);
+	LevelOfDetailEnum getDetailLevel();
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/LevelOfDetail.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/LevelOfDetail.cpp
@@ -1,1 +1,18 @@
 #include "../../headers/data/LevelOfDetail.h"
+
+LevelOfDetail::LevelOfDetail(float distanceFromCamera)
+{
+	if (distanceFromCamera > highDetailMaxDistance)
+	{
+		detailLevel = LevelOfDetailEnum::High;
+	}
+	else
+	{
+		detailLevel = LevelOfDetailEnum::Low;
+	}
+}
+
+LevelOfDetailEnum LevelOfDetail::getDetailLevel()
+{
+	return detailLevel;
+}
