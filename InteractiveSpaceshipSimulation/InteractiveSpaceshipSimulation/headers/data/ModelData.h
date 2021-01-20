7a448b689acf058e2ba5f3ee5fcf9c0871e45beb
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-20 18:46:55 +0100

    Add simplified model field to ModelData

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/ModelData.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/ModelData.h
@@ -8,13 +8,18 @@ class ModelData
 {
 private:
 	obj::Model model;
+	obj::Model simplifiedModel;
 	glm::vec3 forward;
 	glm::vec3 top;
 
 public:
 	ModelData(obj::Model &model, glm::vec3 modelForward, glm::vec3 modelTop);
 
+	ModelData(obj::Model& model, obj::Model& simplifiedModel, glm::vec3 modelForward, glm::vec3 modelTop);
+
+
 	obj::Model getModel();
+	obj::Model getSimplifiedModel();
 
 	glm::vec3 getForward();
 	glm::vec3 getTop();
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/ModelData.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/ModelData.cpp
@@ -5,11 +5,25 @@ ModelData::ModelData(obj::Model& model, glm::vec3 modelForward, glm::vec3 modelT
 	this->model = model;
 	this->forward = modelForward;
 	this->top = modelTop;
+	this->simplifiedModel = model;
+}
+
+ModelData::ModelData(obj::Model& model, obj::Model& simplifiedModel, glm::vec3 modelForward, glm::vec3 modelTop)
+{
+	this->model = model;
+	this->forward = modelForward;
+	this->top = modelTop;
+	this->simplifiedModel = simplifiedModel;
 }
 
 obj::Model ModelData::getModel()
 {
 	return this->model;
+}
+
+obj::Model ModelData::getSimplifiedModel()
+{
+	return simplifiedModel;
 }
 
 glm::vec3 ModelData::getForward()
