40e4468bff20bd8274237b3ee030c371baa4427f
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-29 20:16:35 +0100

    Add Bloom ctor

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/bloom/Bloom.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/bloom/Bloom.h
@@ -1,5 +1,22 @@
 #pragma once
+
+#include "glew.h"
+#include "freeglut.h"
+
+#include "ScreenQuad.h"
+#include "blur/Blur.h"
+#include "hdr/HDRFramebuffer.h"
+
 class Bloom
 {
+	Bloom(unsigned int windowWidth, unsigned int windowHeight, GLuint programBlur, GLuint programBloomFinalBlend);
+public:
+private:
+	GLuint programBloomFinalBlend;
+	float exposure;
+	float gamma;
+	ScreenQuad screenQuad;
+	Blur blur;
+	HDRFramebuffer hdrFramebuffer;
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/bloom/Bloom.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/bloom/Bloom.cpp
@@ -1,1 +1,9 @@
 #include "../../../headers/graphics_techniques/bloom/Bloom.h"
+
+Bloom::Bloom(unsigned int windowWidth, unsigned int windowHeight, GLuint programBlur, GLuint programBloomFinalBlend)
+	: exposure(3.2f), gamma(1.0f), programBloomFinalBlend(programBloomFinalBlend), screenQuad(), 
+	blur(windowWidth, windowHeight, programBlur), hdrFramebuffer(windowWidth, windowHeight)
+	
+{
+
+}
