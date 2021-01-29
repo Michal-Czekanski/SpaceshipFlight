4110a04e9ff0befef311d46b8985271a6b102b5e
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-29 19:14:55 +0100

    Add ScreenQuad ctor

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/bloom/ScreenQuad.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/bloom/ScreenQuad.h
@@ -1,5 +1,22 @@
 #pragma once
+
+#include "glew.h"
+#include "freeglut.h"
+
 class ScreenQuad
 {
+public:
+	ScreenQuad();
+private:
+	unsigned int quadVAO;
+	float quadVertices[4 * 5] = {
+		// positions        // texture Coords
+		-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+		-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+		1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+		1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+	};
+
+
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/bloom/ScreenQuad.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/bloom/ScreenQuad.cpp
@@ -1,1 +1,16 @@
 #include "../../../headers/graphics_techniques/bloom/ScreenQuad.h"
+
+ScreenQuad::ScreenQuad()
+{
+    unsigned int quadVBO;
+    // setup plane VAO
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+}
