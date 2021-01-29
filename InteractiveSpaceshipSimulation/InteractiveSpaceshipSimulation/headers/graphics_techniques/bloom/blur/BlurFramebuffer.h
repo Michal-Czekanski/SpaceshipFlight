267ef4c34ab4714cf30da8d2fa86707e8a48644e
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-29 19:33:01 +0100

    Add BlurFramebuffer ctor

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/bloom/blur/BlurFramebuffer.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/bloom/blur/BlurFramebuffer.h
@@ -1,5 +1,17 @@
 #pragma once
+
+#include "glew.h"
+#include "freeglut.h"
+#include <iostream>
+
 class BlurFramebuffer
 {
+public:
+	BlurFramebuffer(unsigned int windowWidth, unsigned int windowHeight);
+private:
+	unsigned int FBOs[2];
+	unsigned int colorBuffers[2];
+
+	void checkIfFramebufferComplete();
 };
 
