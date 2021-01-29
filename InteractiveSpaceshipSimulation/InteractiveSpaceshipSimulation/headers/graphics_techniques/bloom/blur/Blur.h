949eb73372ed816c9db2d58fb55200195f9d377a
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-29 19:53:47 +0100

    Add Blur ctor

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/bloom/blur/Blur.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/bloom/blur/Blur.h
@@ -1,5 +1,16 @@
 #pragma once
+
+#include "glew.h"
+#include "freeglut.h"
+#include "BlurFramebuffer.h"
+
 class Blur
 {
+public:
+	Blur(unsigned int windowWidth, unsigned int windowHeight, GLuint programBlur);
+private:
+	int blurAmount;
+	GLuint programBlur;
+	BlurFramebuffer framebuffer;
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/bloom/blur/Blur.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/bloom/blur/Blur.cpp
@@ -1,1 +1,6 @@
 #include "../../../../headers/graphics_techniques/bloom/blur/Blur.h"
+
+Blur::Blur(unsigned int windowWidth, unsigned int windowHeight, GLuint programBlur):
+	blurAmount(10), programBlur(programBlur), framebuffer(windowWidth, windowHeight)
+{
+}
