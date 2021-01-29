85afb99e827138c790de3f51f6aa8fe8329faa5e
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-29 19:58:15 +0100

    Add blurring method

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/bloom/blur/Blur.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/bloom/blur/Blur.h
@@ -3,14 +3,16 @@
 #include "glew.h"
 #include "freeglut.h"
 #include "BlurFramebuffer.h"
+#include "../ScreenQuad.h"
 
 class Blur
 {
 public:
 	Blur(unsigned int windowWidth, unsigned int windowHeight, GLuint programBlur);
+	void blur(ScreenQuad screenQuad, GLuint brightLightsTexture);
 private:
 	int blurAmount;
 	GLuint programBlur;
-	BlurFramebuffer framebuffer;
+	BlurFramebuffer blurFramebuffer;
 };
 
