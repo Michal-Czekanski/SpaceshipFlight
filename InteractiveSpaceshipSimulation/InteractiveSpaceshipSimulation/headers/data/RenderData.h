c6498b42850170464fc6a6ac02dbbf3c8a3a1816
Michael-Czekanski <michal.cz2202@gmail.com>
2021-01-31 20:01:05 +0100

    Implement RenderData cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/RenderData.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/RenderData.h
@@ -1,5 +1,18 @@
 #pragma once
+
+#include "utils//Render_Utils.h"
+#include "data/ModelData.h"
+
 class RenderData
 {
+public:
+	RenderData(ModelData& modelData);
+	Core::RenderContext& getRenderContext();
+	Core::RenderContext& getSimplifiedRenderContext();
+	const ModelData& getModelData();
+private:
+	Core::RenderContext renderContext;
+	Core::RenderContext simplifiedRenderContext;
+	const ModelData modelData;
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/RenderData.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/RenderData.cpp
@@ -1,1 +1,24 @@
 #include "data/RenderData.h"
+
+RenderData::RenderData(ModelData& modelData): renderContext(), simplifiedRenderContext(), modelData(modelData)
+{
+	obj::Model model = modelData.getModel();
+	renderContext.initFromOBJ(model);
+	obj::Model simplifiedModel = modelData.getSimplifiedModel();
+	simplifiedRenderContext.initFromOBJ(simplifiedModel);
+}
+
+Core::RenderContext& RenderData::getRenderContext()
+{
+	return renderContext;
+}
+
+Core::RenderContext& RenderData::getSimplifiedRenderContext()
+{
+	return simplifiedRenderContext;
+}
+
+const ModelData& RenderData::getModelData()
+{
+	return modelData;
+}
