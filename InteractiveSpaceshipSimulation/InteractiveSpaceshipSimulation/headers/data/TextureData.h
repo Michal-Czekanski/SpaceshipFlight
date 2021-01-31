e84d55f57370d4ea34616f6fe9a359c9d3eb003d
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-01 00:08:39 +0100

    Implement TextureData cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/TextureData.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/TextureData.h
@@ -1,5 +1,18 @@
 #pragma once
+
+#include "glew.h"
+#include "freeglut.h"
+
 class TextureData
 {
+public:
+	TextureData(GLuint texture, GLuint textureNormal);
+	
+	GLuint getTexture();
+	GLuint getTextureNormal();
+
+private:
+	GLuint texture;
+	GLuint textureNormal;
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/TextureData.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/TextureData.cpp
@@ -1,1 +1,16 @@
 #include "data/TextureData.h"
+
+TextureData::TextureData(GLuint texture, GLuint textureNormal):
+	texture(texture), textureNormal(textureNormal)
+{
+}
+
+GLuint TextureData::getTexture()
+{
+	return texture;
+}
+
+GLuint TextureData::getTextureNormal()
+{
+	return textureNormal;
+}
