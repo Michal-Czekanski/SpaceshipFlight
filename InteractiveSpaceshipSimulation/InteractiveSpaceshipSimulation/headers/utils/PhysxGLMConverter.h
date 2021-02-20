8f2330e96aa9bb0050a5b67b32ebc15098adae1e
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-20 23:09:54 +0100

    Add method converting PxVec3 to glm vec3

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/utils/PhysxGLMConverter.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/utils/PhysxGLMConverter.h
@@ -1,5 +1,14 @@
 #pragma once
+
+#include "glm.hpp"
+#include "ext.hpp"
+#include "PxPhysicsAPI.h"
+
+using namespace physx;
+
 class PhysxGLMConverter
 {
+public:
+	static glm::vec3 PxVec3ToVec3(PxVec3 pxVec3);
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/utils/PhysxGLMConverter.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/utils/PhysxGLMConverter.cpp
@@ -1,1 +1,6 @@
 #include "utils/PhysxGLMConverter.h"
+
+glm::vec3 PhysxGLMConverter::PxVec3ToVec3(PxVec3 pxVec3)
+{
+	return glm::vec3(pxVec3.x, pxVec3.y, pxVec3.z);
+}
