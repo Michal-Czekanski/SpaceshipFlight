455eb11326ed7f96c4814c03098f47d5b7bbcddb
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-21 00:29:23 +0100

    Add attributes and getters in PhysicsOptions cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/PhysicsOptions.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/PhysicsOptions.h
@@ -1,5 +1,17 @@
 #pragma once
+
+#include "PxPhysicsAPI.h"
+
 class PhysicsOptions
 {
+public:
+	const float getGravity();
+	const float getStepTime();
+	physx::PxSceneDesc& getSceneDesc();
+
+private:
+	const float gravity = 0.0f;
+	const float stepTime = 1 / 60.0f;
+	physx::PxSceneDesc scnDesc;
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/physics/PhysicsOptions.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/physics/PhysicsOptions.cpp
@@ -1,1 +1,18 @@
 #include "physics/PhysicsOptions.h"
+
+
+
+const float PhysicsOptions::getGravity()
+{
+	return gravity;
+}
+
+const float PhysicsOptions::getStepTime()
+{
+	return stepTime;
+}
+
+physx::PxSceneDesc& PhysicsOptions::getSceneDesc()
+{
+	return scnDesc;
+}
