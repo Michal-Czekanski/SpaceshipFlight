e8fd269cb51b638172891f63ac6b577dfa928e14
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-21 00:51:44 +0100

    Implement PhysicsOptions cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/data/PhysicsOptions.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/data/PhysicsOptions.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "PxPhysicsAPI.h"
+#include "physics/data/SimulationEventCallback.h"
+#include "physics/data/simulationFilterShader.h"
 
 class PhysicsOptions
 {
 public:
+	PhysicsOptions();
 	const float getGravity();
 	const float getStepTime();
 	physx::PxSceneDesc& getSceneDesc();
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/physics/data/PhysicsOptions.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/physics/data/PhysicsOptions.cpp
@@ -2,6 +2,16 @@
 
 
 
+PhysicsOptions::PhysicsOptions(): scnDesc(PxTolerancesScale())
+{
+	scnDesc.gravity = PxVec3(0.0f, -gravity, 0.0f);
+    scnDesc.cpuDispatcher = PxDefaultCpuDispatcherCreate(2);
+    scnDesc.filterShader = simulationFilterShader;
+    scnDesc.kineKineFilteringMode = PxPairFilteringMode::eKEEP; // So kin-kin contacts with be reported
+    scnDesc.staticKineFilteringMode = PxPairFilteringMode::eKEEP; // So static-kin constacts will be reported
+    scnDesc.simulationEventCallback = new SimulationEventCallback();
+}
+
 const float PhysicsOptions::getGravity()
 {
 	return gravity;
