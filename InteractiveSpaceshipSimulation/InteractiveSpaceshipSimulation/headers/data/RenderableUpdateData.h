12e18a5aaa119ad07dd0c46f4c36da9f86bebb98
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-20 23:57:28 +0100

    Implement RenderableUpdateData cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/RenderableUpdateData.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/data/RenderableUpdateData.h
@@ -1,5 +1,17 @@
 #pragma once
+
+#include "utils/PhysxGLMConverter.h"
+
 class RenderableUpdateData
 {
+public:
+	RenderableUpdateData(PxTransform data);
+	glm::quat& getRotation();
+	glm::vec3& getPosition();
+	glm::mat4& getModelMatrix();
+private:
+	glm::quat rotation;
+	glm::vec3 position;
+	glm::mat4 modelMatrix;
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/RenderableUpdateData.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/data/RenderableUpdateData.cpp
@@ -1,1 +1,22 @@
 #include "data/RenderableUpdateData.h"
+
+RenderableUpdateData::RenderableUpdateData(PxTransform data):
+	rotation(PhysxGLMConverter::PxQuatToQuat(data.q)), position(PhysxGLMConverter::PxVec3ToVec3(data.p)),
+	modelMatrix(PhysxGLMConverter::PxMat44ToMat4(data))
+{
+}
+
+glm::quat& RenderableUpdateData::getRotation()
+{
+	return rotation;
+}
+
+glm::vec3& RenderableUpdateData::getPosition()
+{
+	return position;
+}
+
+glm::mat4& RenderableUpdateData::getModelMatrix()
+{
+	return modelMatrix;
+}
