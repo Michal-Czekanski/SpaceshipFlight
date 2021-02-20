18031306ff2f333039d214d07176fcee2e8abbab
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-20 23:17:49 +0100

    Add more methods converting from glm to physx

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/utils/PhysxGLMConverter.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/utils/PhysxGLMConverter.h
@@ -6,9 +6,14 @@
 
 using namespace physx;
 
+/// <summary>
+/// Static class capable of converting values from glm to physx and in reverse.
+/// </summary>
 class PhysxGLMConverter
 {
 public:
 	static glm::vec3 PxVec3ToVec3(PxVec3 pxVec3);
+	static PxVec3 vec3ToPxVec3(glm::vec3 vec3);
+	static PxVec2 vec2ToPxVec2(glm::vec2 vec2);
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/utils/PhysxGLMConverter.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/utils/PhysxGLMConverter.cpp
@@ -4,3 +4,13 @@ glm::vec3 PhysxGLMConverter::PxVec3ToVec3(PxVec3 pxVec3)
 {
 	return glm::vec3(pxVec3.x, pxVec3.y, pxVec3.z);
 }
+
+PxVec3 PhysxGLMConverter::vec3ToPxVec3(glm::vec3 vec3)
+{
+	return PxVec3(vec3.x, vec3.y, vec3.z);
+}
+
+PxVec2 PhysxGLMConverter::vec2ToPxVec2(glm::vec2 vec2)
+{
+	return PxVec2(vec2.x, vec2.y);
+}
