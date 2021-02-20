bbc59c09c27bb255821110bc720989f47371f0c8
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-21 00:38:13 +0100

    Add SimulationEventCallback cls

---
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/SimulationEventCallback.cpp
@@ -0,0 +1,25 @@
+#include "SimulationEventCallback.h"
+
+void SimulationEventCallback::onConstraintBreak(PxConstraintInfo* constraints, PxU32 count)
+{
+}
+
+void SimulationEventCallback::onWake(PxActor** actors, PxU32 count)
+{
+}
+
+void SimulationEventCallback::onSleep(PxActor** actors, PxU32 count)
+{
+}
+
+void SimulationEventCallback::onContact(const PxContactPairHeader& pairHeader, const PxContactPair* pairs, PxU32 nbPairs)
+{
+}
+
+void SimulationEventCallback::onTrigger(PxTriggerPair* pairs, PxU32 count)
+{
+}
+
+void SimulationEventCallback::onAdvance(const PxRigidBody* const* bodyBuffer, const PxTransform* poseBuffer, const PxU32 count)
+{
+}
---
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/SimulationEventCallback.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "PxPhysicsAPI.h"
+
+using namespace physx;
+
+/// <summary>
+/// Simulation events processor
+/// </summary>
+class SimulationEventCallback :
+    public PxSimulationEventCallback
+{
+    // Inherited via PxSimulationEventCallback
+    virtual void onConstraintBreak(PxConstraintInfo* constraints, PxU32 count) override;
+    virtual void onWake(PxActor** actors, PxU32 count) override;
+    virtual void onSleep(PxActor** actors, PxU32 count) override;
+    virtual void onContact(const PxContactPairHeader& pairHeader, const PxContactPair* pairs, PxU32 nbPairs) override;
+    virtual void onTrigger(PxTriggerPair* pairs, PxU32 count) override;
+    virtual void onAdvance(const PxRigidBody* const* bodyBuffer, const PxTransform* poseBuffer, const PxU32 count) override;
+};
+
