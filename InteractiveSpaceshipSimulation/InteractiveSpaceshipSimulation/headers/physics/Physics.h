7ac72969c6a25cec033c08bfc729d15a6e533cd7
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-21 01:55:37 +0100

    Add fields in Physics cls

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/Physics.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/Physics.h
@@ -1,5 +1,18 @@
 #pragma once
+
+#include "PxPhysicsAPI.h"
+
+using namespace physx;
+
 class Physics
 {
+public:
+private:
+	static Physics instance;
+	const float stepTime;
+	float timeToProcess = 0.0f;
+	const float maxDeltaTime = 1.0f;
+	PxPhysics* pxPhysics;
+	PxScene* pxScene;
 };
 
