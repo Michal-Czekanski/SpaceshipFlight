2220face0dc2e48c04af0c5e9188e216887b7dbb
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-21 13:04:19 +0100

    Remove code duplication from RigidbodyFactory

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/RigidbodyFactory.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/RigidbodyFactory.h
@@ -8,5 +8,9 @@ class RigidbodyFactory
 public:
 	static PxRigidDynamic* createShipRigidbody(glm::vec3 position, glm::quat rotation, void* shipPointer);
 	static PxRigidDynamic* createAsteroidRigidbody(glm::vec3 position, glm::quat rotation, void* asteroidPointer);
+
+private:
+	static PxRigidDynamic* createDynamicRigidbody(glm::vec3 position, glm::quat rotation, void* userData, 
+		PxMaterial* mat, PxGeometry& geometry);
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/physics/RigidbodyFactory.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/physics/RigidbodyFactory.cpp
@@ -2,38 +2,30 @@
 
 PxRigidDynamic* RigidbodyFactory::createShipRigidbody(glm::vec3 position, glm::quat rotation, void* shipPointer)
 {
-	PxPhysics* pxPhysics = Physics::getInstance()->getPxPhysics();
-	PxScene* pxScene = Physics::getInstance()->getPxScene();
-
-	PxRigidDynamic* shipBody = pxPhysics->createRigidDynamic(
-		PxTransform(PhysxGLMConverter::vec3ToPxVec3(position), PhysxGLMConverter::quatToPxQuat(rotation)));
-	
-	PxMaterial* shipMaterial = pxPhysics->createMaterial(0.5, 0.5, 0.6);
-
-	PxGeometry shipGeometry = PxBoxGeometry(1, 1, 1); // TODO
-	PxShape* shipShape = pxPhysics->createShape(shipGeometry, *shipMaterial);
-	shipBody->attachShape(*shipShape);
-	shipShape->release();
-	shipBody->userData = shipPointer;
-	pxScene->addActor(*shipBody);
-	return shipBody;
+	PxMaterial* material = Physics::getInstance()->getPxPhysics()->createMaterial(0.5, 0.5, 0.6);
+	PxGeometry geometry = PxBoxGeometry(1, 1, 1); // TODO
+	return createDynamicRigidbody(position, rotation, shipPointer, material, geometry);
 }
 
 PxRigidDynamic* RigidbodyFactory::createAsteroidRigidbody(glm::vec3 position, glm::quat rotation, void* asteroidPointer)
 {
+	PxMaterial* material = Physics::getInstance()->getPxPhysics()->createMaterial(0.5, 0.5, 0.6);
+	PxGeometry geometry = PxSphereGeometry(1); // TODO
+	return createDynamicRigidbody(position, rotation, asteroidPointer, material, geometry);
+}
+
+PxRigidDynamic* RigidbodyFactory::createDynamicRigidbody(glm::vec3 position, glm::quat rotation, void* userData, PxMaterial* mat, PxGeometry& geometry)
+{
 	PxPhysics* pxPhysics = Physics::getInstance()->getPxPhysics();
 	PxScene* pxScene = Physics::getInstance()->getPxScene();
 
-	PxRigidDynamic* asteroidBody = pxPhysics->createRigidDynamic(
+	PxRigidDynamic* body = pxPhysics->createRigidDynamic(
 		PxTransform(PhysxGLMConverter::vec3ToPxVec3(position), PhysxGLMConverter::quatToPxQuat(rotation)));
 
-	PxMaterial* asteroidMaterial = pxPhysics->createMaterial(0.5, 0.5, 0.6);
-
-	PxGeometry asteroidGeometry = PxSphereGeometry(1); // TODO
-	PxShape* asteroidShape = pxPhysics->createShape(asteroidGeometry, *asteroidMaterial);
-	asteroidBody->attachShape(*asteroidShape);
-	asteroidShape->release();
-	asteroidBody->userData = asteroidPointer;
-	pxScene->addActor(*asteroidBody);
-	return asteroidBody;
+	PxShape* shape = pxPhysics->createShape(geometry, *mat);
+	body->attachShape(*shape);
+	shape->release();
+	body->userData = userData;
+	pxScene->addActor(*body);
+	return body;
 }
