df403032cc901f1da0428a8bd01a96344e54f188
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-21 02:17:44 +0100

    Add Physics getInstance and ctor methods

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/Physics.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/Physics.h
@@ -1,20 +1,25 @@
 #pragma once
 
-#include "PxPhysicsAPI.h"
-
-using namespace physx;
+#include "data/PhysicsOptions.h"
 
 class Physics
 {
 public:
+	Physics* getInstance();
 	PxPhysics* getPxPhysics();
 	PxScene* getPxScene();
 private:
-	static Physics instance;
+	static Physics* instance;
+	Physics(PhysicsOptions& options);
 	const float stepTime;
 	float timeToProcess = 0.0f;
 	const float maxDeltaTime = 1.0f;
 	PxPhysics* pxPhysics;
 	PxScene* pxScene;
+
+	PxDefaultAllocator		allocator;
+	PxDefaultErrorCallback	errorCallback;
+	PxFoundation* foundation = nullptr;
+	PxDefaultCpuDispatcher* dispatcher = nullptr;
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/physics/Physics.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/physics/Physics.cpp
@@ -1,5 +1,17 @@
 #include "physics/Physics.h"
 
+Physics* Physics::instance = nullptr;
+
+Physics* Physics::getInstance()
+{
+	if (!instance) 
+	{
+		PhysicsOptions options;
+		instance = new Physics(options);
+	}
+	return instance;
+}
+
 PxPhysics* Physics::getPxPhysics()
 {
 	return pxPhysics;
@@ -8,4 +20,12 @@ PxPhysics* Physics::getPxPhysics()
 PxScene* Physics::getPxScene()
 {
 	return pxScene;
+}
+
+Physics::Physics(PhysicsOptions& options): stepTime(options.getStepTime())
+{
+	dispatcher = (PxDefaultCpuDispatcher*)options.getSceneDesc().cpuDispatcher;
+	foundation = PxCreateFoundation(PX_PHYSICS_VERSION, allocator, errorCallback);
+	pxPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *foundation, options.getSceneDesc().getTolerancesScale(), true);
+	pxScene = pxPhysics->createScene(options.getSceneDesc());
 }
