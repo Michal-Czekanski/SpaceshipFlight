9fefdaf399de008d08da37e1fea0dc13ebcc29ce
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-21 12:00:26 +0100

    Add method creating ship physics body

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/RigidbodyFactory.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/physics/RigidbodyFactory.h
@@ -1,5 +1,11 @@
 #pragma once
+
+#include "utils/PhysxGLMConverter.h"
+#include "physics/Physics.h"
+
 class RigidbodyFactory
 {
+public:
+	static PxRigidDynamic* createShipRigidbody(glm::vec3 position, glm::quat rotation, void* shipPointer);
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/physics/RigidbodyFactory.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/physics/RigidbodyFactory.cpp
@@ -1,1 +1,21 @@
 #include "physics/RigidbodyFactory.h"
+
+PxRigidDynamic* RigidbodyFactory::createShipRigidbody(glm::vec3 position, glm::quat rotation, void* shipPointer)
+{
+	Physics* p = Physics::getInstance();
+	PxPhysics* pxP = p->getPxPhysics();
+	PxScene* pxS = p->getPxScene();
+
+	PxRigidDynamic* shipBody = pxP->createRigidDynamic(
+		PxTransform(PhysxGLMConverter::vec3ToPxVec3(position), PhysxGLMConverter::quatToPxQuat(rotation)));
+	
+	PxMaterial* shipMaterial = pxP->createMaterial(0.5, 0.5, 0.6);
+
+	PxGeometry shipGeometry = PxBoxGeometry(1, 1, 1); // TODO
+	PxShape* shipShape = pxP->createShape(shipGeometry, *shipMaterial);
+	shipBody->attachShape(*shipShape);
+	shipShape->release();
+	shipBody->userData = shipPointer;
+	pxS->addActor(*shipBody);
+	return shipBody;
+}
