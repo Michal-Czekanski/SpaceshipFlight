761fced5ac3b02c91186add8d416502dd1822328
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-24 01:06:26 +0100

    Add Particle ctor

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/particles/Particle.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/particles/Particle.h
@@ -6,11 +6,14 @@
 class Particle
 {
 public:
-
+	Particle(glm::vec3 position, glm::vec4 color, glm::vec3 velocity);
 private:
 	glm::vec3 position;
 	glm::vec4 color;
-	float life;
 	glm::vec3 velocity;
+	const float maxLife = 1.0f;
+	const float minLife = 0.0f;
+	float life;
+
 };
 
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/particles/Particle.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/particles/Particle.cpp
@@ -1,1 +1,7 @@
 #include "graphics_techniques/particles/Particle.h"
+
+Particle::Particle(glm::vec3 position, glm::vec4 color, glm::vec3 velocity): 
+	position(position), color(color), velocity(velocity)
+{
+	life = maxLife;
+}
