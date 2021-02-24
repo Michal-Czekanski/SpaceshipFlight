9ad24dbe21255b36d627e2e3d62056ea52f919ed
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-24 15:18:51 +0100

    Add ParticleGenerator ctor

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/particles/ParticleGenerator.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/particles/ParticleGenerator.h
@@ -1,16 +1,35 @@
 #pragma once
 
+#include "glew.h"
+#include "freeglut.h"
 #include "graphics_techniques/particles/Particle.h"
+#include <vector>
 
 class ParticleGenerator
 {
 public:
+	ParticleGenerator(glm::vec3 posInParent, glm::vec3 parentPosition, glm::vec3 generationDir, glm::quat parentRotation,
+		const int maxParticles,	float generationAngle, unsigned int programId, unsigned int textureId);
 private:
 	const int maxParticles;
-	Particle* particles;
+	std::vector<Particle> particles;
 	glm::vec3 posInParent;
 	glm::vec3 generationDir;
+	glm::vec3 worldPosition;
 	float generationAngle;
 	unsigned int programId;
 	unsigned int textureId;
+
+	GLuint vao;
+	GLuint instanceModelMatrixesBuffer;
+
+	void initVAO();
+
+
+	const float billboard_vertexes[3 * 4] = {
+	 -0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 -0.5f, 0.5f, 0.0f,
+	 0.5f, 0.5f, 0.0f,
+	};
 };
--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/particles/ParticleGenerator.cpp
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/src/graphics_techniques/particles/ParticleGenerator.cpp
@@ -1,1 +1,56 @@
 #include "graphics_techniques/particles/ParticleGenerator.h"
+
+ParticleGenerator::ParticleGenerator(glm::vec3 posInParent, glm::vec3 parentPosition, glm::vec3 generationDir, 
+	glm::quat parentRotation, const int maxParticles, float generationAngle, 
+	unsigned int programId, unsigned int textureId):
+	posInParent(posInParent), worldPosition(parentPosition + posInParent), maxParticles(maxParticles),
+	generationAngle(generationAngle), programId(programId), textureId(textureId), generationDir(generationDir),
+	particles()
+{
+	initVAO();
+}
+
+void ParticleGenerator::initVAO()
+{
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//
+	GLuint billboardVBO;
+	glGenBuffers(1, &billboardVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, billboardVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(billboard_vertexes), billboard_vertexes, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+	//
+	GLuint colorsVBO;
+	glGenBuffers(1, &colorsVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, colorsVBO);
+	glBufferData(GL_ARRAY_BUFFER, maxParticles * 4 * sizeof(float), NULL, GL_STREAM_DRAW);
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+	//
+	glGenBuffers(1, &instanceModelMatrixesBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceModelMatrixesBuffer);
+	glBufferData(GL_ARRAY_BUFFER, maxParticles * sizeof(glm::mat4), NULL, GL_STREAM_DRAW);
+
+	glEnableVertexAttribArray(3);
+	glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0);
+	glEnableVertexAttribArray(4);
+	glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4)));
+	glEnableVertexAttribArray(5);
+	glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(2 * sizeof(glm::vec4)));
+	glEnableVertexAttribArray(6);
+	glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(3 * sizeof(glm::vec4)));
+
+	glVertexAttribDivisor(3, 1);
+	glVertexAttribDivisor(4, 1);
+	glVertexAttribDivisor(5, 1);
+	glVertexAttribDivisor(6, 1);
+
+	glBindVertexArray(0);
+}
