34b375c744d1a2ec6dd151062e578b96ce547171
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-24 01:11:32 +0100

    Add ParticlesGenerator fields

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/particles/ParticleGenerator.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/graphics_techniques/particles/ParticleGenerator.h
@@ -1,5 +1,16 @@
 #pragma once
+
+#include "graphics_techniques/particles/Particle.h"
+
 class ParticleGenerator
 {
+public:
+private:
+	const int maxParticles;
+	Particle* particles;
+	glm::vec3 posInParent;
+	glm::vec3 generationDir;
+	float generationAngle;
+	unsigned int programId;
+	unsigned int textureId;
 };
-
