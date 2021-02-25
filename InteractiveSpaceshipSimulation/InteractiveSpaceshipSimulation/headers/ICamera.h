f53e2d0d0cd4dc7934fd4168c2a52d5a055aec41
Michael-Czekanski <michal.cz2202@gmail.com>
2021-02-25 16:15:58 +0100

    Add ICamera methods

--- InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/ICamera.h
+++ InteractiveSpaceshipSimulation/InteractiveSpaceshipSimulation/headers/ICamera.h
@@ -1,6 +1,13 @@
 #pragma once
+
+#include "glm.hpp"
+#include "ext.hpp"
+
 class ICamera
 {
-
+public:
+	virtual glm::quat getRotation() = 0;
+	virtual glm::mat4 getCameraMatrix() = 0;
+	virtual glm::vec3 getCamPos() = 0;
 };
 
